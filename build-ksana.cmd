2e046f7d3be36502a0bbe4da9123b9482e3cb071
yapcheahshen <yapcheahshen@gmail.com>
2015-08-25 15:54:22 +0800

    update ksana bundle

--- build-ksana.cmd
+++ build-ksana.cmd
@@ -1,1 +1,1 @@
-browserify --bare -r ksana-database -r ksana-search -r ksana-jsonrom -r ksana-analyzer -u react-native> ksana-bundle.js
+browserify --bare -r ksana-database -r ksana-search -r ksana-jsonrom -r ksana-analyzer -r ksana-simple-api -r ksana-simple-api -u react-native> ksana-bundle.js
